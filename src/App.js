38b54673e0e63dff1e0cc9018b4756e6246345bd
arovillard <arovillard@gmail.com>
2018-05-07 15:07:48 -0400

    update example on app

--- src/App.js
+++ src/App.js
@@ -9,18 +9,62 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Styled-Grid-Responsie Demo</h1>
         </header>
-        <Grid rev>
-          <GridItem col={1/2}>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
+        <Grid gutter={50}>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 1</p>
           </GridItem>
-          <GridItem col={1/2}>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 2</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 3</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 4</p>
+          </GridItem>
+        </Grid>
+        <Grid full rev>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 1</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 2</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 3</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 4</p>
+          </GridItem>
+        </Grid>
+        <Grid middle center>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 1</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <h1>Item 2</h1>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 3</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 4</p>
+          </GridItem>
+        </Grid>
+        <Grid bottom>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 1</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <h1>Item 2</h1>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 3</p>
+          </GridItem>
+          <GridItem media={{ phone: 1, tablet: 1 / 2 }} col={1 / 4}>
+            <p>Item 4</p>
           </GridItem>
         </Grid>
       </div>
